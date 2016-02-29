af641fd3d4bb71e769bb1e7d3b68a1fd977cfe68
b-fuze <b-fuze.root@live.com>
2016-02-29 17:34:25 +0200

    Add aur-refactor, update aur-themify and aur-page with isChannel, isSearch, isList

--- src/mods/core/aur-page.mod.js
+++ src/mods/core/aur-page.mod.js
@@ -7,6 +7,8 @@
   regs.interface = {
     isHome:    /^https?:\/\/(www\.)?animeultima.io\/?(#[^]+)?$/.test(url),
     isEpisode: /^https?:\/\/(www\.)?animeultima.io\/[^]+-episode-[\d\.]+(?:-english-[sd]ubbed(?:-video-mirror-\d+-[^]+)?)?\/?(#[^]+)?$/.test(url),
-    isChannel: /^https?:\/\/(www\.)?animeultima.io\/(?:watch\/[^]+-english-subbed-dubbed-online)\/?(#[^]+)?$/.test(url)
+    isChannel: /^https?:\/\/(www\.)?animeultima.io\/(?:watch\/[^]+-english-subbed-dubbed-online)\/?(#[^]+)?$/.test(url),
+    isSearch:  /^https?:\/\/(www\.)?animeultima.io\/search.html(?:\?searchquery=?[^]*)?(#[^]+)?$/.test(url),
+    isList:    /^https?:\/\/(www\.)?animeultima.io\/watch-anime\/?(#[^]+)?$/.test(url)
   };
 })();
