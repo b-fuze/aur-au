f288353c7d64d7b460b28eae7a956192122f9713
b-fuze <b-fuze.root@live.com>
2016-03-24 02:08:24 +0200

    Finish aur-details, update builder - broken mod won't crash, no IIFE, aur-ui WIP, themify, fixes

--- src/mods/core/aur-page.mod.js
+++ src/mods/core/aur-page.mod.js
@@ -1,14 +1,12 @@
 // aur-page test for different page types
 
-(function() {
-  var regs = AUR.register("aur-page");
-  var url  = document.location.toString();
-  
-  regs.interface = {
-    isHome:    /^https?:\/\/(www\.)?animeultima.io\/?(#[^]+)?$/.test(url),
-    isEpisode: /^https?:\/\/(www\.)?animeultima.io\/[^]+-episode-[\d\.]+(?:-english-[sd]ubbed(?:-video-mirror-\d+-[^]+)?)?\/?(#[^]+)?$/.test(url),
-    isChannel: /^https?:\/\/(www\.)?animeultima.io\/(?:watch\/[^]+-english-subbed-dubbed-online)\/?(#[^]+)?$/.test(url),
-    isSearch:  /^https?:\/\/(www\.)?animeultima.io\/search.html(?:\?searchquery=?[^]*)?(#[^]+)?$/.test(url),
-    isList:    /^https?:\/\/(www\.)?animeultima.io\/watch-anime\/?(#[^]+)?$/.test(url)
-  };
-})();
+var regs = AUR.register("aur-page");
+var url  = document.location.toString();
+
+regs.interface = {
+  isHome:    /^https?:\/\/(www\.)?animeultima\.io\/?(#[^]+)?$/.test(url),
+  isEpisode: /^https?:\/\/(www\.)?animeultima\.io\/[^]+-episode-[\d\.]+(?:-english-[sd]ubbed(?:-video-mirror-\d+-[^]+)?)?\/?(#[^]+)?$/.test(url),
+  isChannel: /^https?:\/\/(www\.)?animeultima\.io\/(?:watch\/[^]+-english-subbed-dubbed-online)(?:\/favorites)?\/?(#[^]+)?$/.test(url),
+  isSearch:  /^https?:\/\/(www\.)?animeultima\.io\/search.html(?:\?searchquery=?[^]*)?(#[^]+)?$/.test(url),
+  isList:    /^https?:\/\/(www\.)?animeultima\.io\/watch-anime(?:-movies)?\/?(#[^]+)?$/.test(url)
+};
