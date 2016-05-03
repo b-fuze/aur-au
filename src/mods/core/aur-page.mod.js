07e67f2fd7e0bd134c030c0ed74ce5d0bc6021a0
b-fuze <b-fuze.root@live.com>
2016-05-03 08:55:15 +0300

    Add mod-toggle, make registering in the builder, add ui props to refactor, header-fix, and the verbose errors in settings, fixes, polish, LCES update

--- src/mods/core/aur-page.mod.js
+++ src/mods/core/aur-page.mod.js
@@ -5,14 +5,15 @@ AUR_VERSION = [0, 1];
 AUR_AUTHORS = ["Mike32 (b-fuze)"];
 AUR_RESTART = true;
 
-var regs = AUR.register("aur-page");
+var regs = reg;
 var url  = document.location.toString();
 
 regs.interface = {
-  isAU:      /^https?:\/\/(www\.)?animeultima\.io(\/[^]*)?$/.test(url),
-  isHome:    /^https?:\/\/(www\.)?animeultima\.io\/?(#[^]+)?$/.test(url),
-  isEpisode: /^https?:\/\/(www\.)?animeultima\.io\/[^]+-episode-[\d\.]+(?:-english-[sd]ubbed(?:-video-mirror-\d+-[^]+)?)?\/?(#[^]+)?$/.test(url),
-  isChannel: /^https?:\/\/(www\.)?animeultima\.io\/(?:watch\/[^]+-english-subbed-dubbed-online)(?:\/favorites)?\/?(#[^]+)?$/.test(url),
-  isSearch:  /^https?:\/\/(www\.)?animeultima\.io\/search.html(?:\?searchquery=?[^]*)?(#[^]+)?$/.test(url),
-  isList:    /^https?:\/\/(www\.)?animeultima\.io\/watch-anime(?:-movies)?\/?(#[^]+)?$/.test(url)
+  isAU:         /^https?:\/\/(www\.)?animeultima\.io(\/[^]*)?$/.test(url),
+  isHome:       /^https?:\/\/(www\.)?animeultima\.io(?:\/+(?:index.php)?)?(#[^]+)?$/.test(url),
+  isEpisode:    /^https?:\/\/(www\.)?animeultima\.io\/+[^]+-episode-[\d\.]+(?:-english-[sd]ubbed(?:-video-mirror-\d+-[^]+)?)?(?:\/+)?(#[^]+)?$/.test(url),
+  isChannel:    /^https?:\/\/(www\.)?animeultima\.io\/+(?:watch\/+[^]+-english-subbed-dubbed-online)(?:\/+)?(#[^]+)?$/.test(url),
+  isChannelFav: /^https?:\/\/(www\.)?animeultima\.io\/+(?:watch\/+[^]+-english-subbed-dubbed-online)\/+favorites\/?(#[^]+)?$/.test(url),
+  isSearch:     /^https?:\/\/(www\.)?animeultima\.io\/+search.html(?:\?searchquery=?[^]*)?(#[^]+)?$/.test(url),
+  isList:       /^https?:\/\/(www\.)?animeultima\.io\/+watch-anime(?:-movies)?\/+?(#[^]+)?$/.test(url)
 };
