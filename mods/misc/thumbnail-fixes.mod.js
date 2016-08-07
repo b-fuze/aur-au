eda9ea49ad56e0dc9288dc3b00e9a9008982e425
unknown <gtsaman@gmail.com>
2016-08-07 20:38:17 +0300

    changed how thumbnail works, should be a little faster now (hope it is not slower ._.)

--- mods/misc/thumbnail-fixes.mod.js
+++ mods/misc/thumbnail-fixes.mod.js
@@ -15,38 +15,46 @@ reg.on("modenable", ini);
 
 
 
-function getThumbsForMainPage(videoItemsBox){
-  if( !videoItemsBox instanceof Element || videoItemsBox.classList.contains("aurMended") )
+function getThumbsForMainPage(videoItemsBox, force) {
+  if ((!videoItemsBox instanceof Element || videoItemsBox.classList.contains("aurMended")) && !force)
     return;
+
+  if (force)
+    console.log("FORCED");
 
   videoItemsBox.classList.add("aurMended");
 
   var videoItems = videoItemsBox.querySelectorAll(".generic-video-item");
 
-  for (var i = videoItems.length - 1; i >= 0; i--) {
-    var videoItem = videoItems[i];
+  if (!videoItems.length)
+    return;
 
-    if( videoItem.querySelectorAll(".title").length ){
-      var link = videoItem.querySelector(".thumb > a"); //episode.jSh(".thumb > a");
+  if (videoItems[0].querySelector(".title")) { //if main page
 
-      if( db( getTitle(link.href) ) )
+    for (var i = videoItems.length - 1; i >= 0; i--) {
+      var videoItem = videoItems[i];
+
+      var link = videoItem.querySelector(".thumb > a");
+
+      if (db( getTitle(link.href) )) //if in database
         functionNameC(db( getTitle(link.href) ), videoItem, "Cache");
-      else
+      else //if not in db
         functionNameA(link.href, videoItem);
 
-    }else{
-      var link = document.querySelector("#pembed iframe").src;
-      if( db( getTitle(document.location.href) ) )
-        functionNameC(db( getTitle(document.location.href) ), videoItem);
-      else
-        functionNameB(link, videoItem, "Cache");
     }
 
+  } else { //if episode page
+    var link = document.querySelector("#pembed iframe").src;
+    if (db( getTitle(document.location.href) )) //if in db
+      functionNameC(db( getTitle(document.location.href) ), videoItems,  "Cache");
+    else //if not in db
+      functionNameB(link, videoItems);
   }
+
 
 }
 
-function functionNameA(url, videoItem){
+function functionNameA(url, videoItem) {
 
   var req = new lcRequest({
     method: "GET",
@@ -54,10 +62,10 @@ function functionNameA(url, videoItem){
     success: function(){
 
       var match = this.response.match(/pembed.+<iframe.+src="([A-Za-z0-9_:/.?=;\-&~#[\]@!$'()*+,%\/]+)".+iframe>/);
-      if ( match && match[1])
+      if (match && match[1]) {
         url = match[1];
-
-      functionNameB(url, videoItem);
+        functionNameB(url, videoItem);
+      }
 
     }
   });
@@ -66,22 +74,22 @@ function functionNameA(url, videoItem){
 }
 
 
-function functionNameB(url, videoItem){
+function functionNameB(url, videoItems) {
 
   var thumbnailUrl;
 
-  if( /auengine\.com/.test(url) ){
+  if (/auengine\.com/.test(url)) {
 
     var videoId = url.match(/file=(\w+)&?/);
-    if( videoId && videoId[1] )
+    if (videoId && videoId[1])
       thumbnailUrl = "http://thumbs.auengine.com/" + videoId[1] + "_b.png";
 
-    functionNameC(thumbnailUrl, videoItem , "AUEngine");
+    functionNameC(thumbnailUrl, videoItems , "AUEngine");
 
-  }else if( /mp4upload\.com/.test(url) ){
+  } else if (/mp4upload\.com/.test(url)) {
 
     videoId = url.match(/embed-(.+)-/);
-    if( videoId && videoId[1] ){
+    if (videoId && videoId[1]) {
       var url = "http://www.mp4upload.com/" + videoId[1];
       var regex = new RegExp('src="(http://www\\d*\\.mp4upload\\.com/\\w/\\d+/'+videoId[1]+'_t\\.jpg)"');
 
@@ -90,56 +98,62 @@ function functionNameB(url, videoItem){
         uri: url,
         success: function(){
           var match = this.response.match(regex);
-          if( match && match[1] )
-            functionNameC(match[1], videoItem, "MP4Upload");
+          if (match && match[1])
+            functionNameC(match[1], videoItems, "MP4Upload");
         }
       })
 
     }
 
-  }else{
+  } else {
     console.log("Mirror not supported");
+  }
+}
+
+
+function functionNameC(thumbnailUrl, videoItems, s) {
+
+  if (!videoItems.length && videoItems.length !== 0) {
+    videoItems = [videoItems];
+  }
+  
+  for(var i = 0; i < videoItems.length; i++){
+    videoItem = videoItems[i];
+    var anchor = videoItem.querySelector(".thumb a");
+    if (anchor)
+      var url = anchor.href;
+    else
+      var url = document.location.href;
+
+    db(getTitle(url), thumbnailUrl);
+
+    console.log(s);
+    var bgImg = videoItem.querySelector(".thumb .bg-image");
+
+    if (!thumbnailUrl)
+      thumbnailUrl = "http://static.cdn.animeultima.tv/images/blank.gif";
+
+    var newBgImg = document.createElement("span");
+    newBgImg.setAttribute("style", "position: absolute; top: 1px; left: 1px; width: 150px; height: 100px; background-size: 150px 100px;");
+    newBgImg.style.backgroundImage = "url(" + thumbnailUrl + ")";
+    newBgImg.className = "newThumbnail";
+
+    bgImg.parentElement.appendChild(newBgImg);
+    bgImg.style.display = "none";
+
+    if (!s)
+      s = "Unknown";
+    var sourceTest = document.createElement("span");
+    sourceTest.className = "thumbnailSource";
+    sourceTest.innerText = s;
+    sourceTest.setAttribute("style", "background: black;font-size: 10px;color: #b1b1b1;position: absolute;bottom: 3px;left: 3px;z-index: 300;padding: 2px;")
+    bgImg.parentElement.appendChild(sourceTest);
   }
 
 }
 
 
-function functionNameC(thumbnailUrl, videoItem, s){
-
-  var anchor = videoItem.querySelector(".thumb a");
-  if( anchor )
-    var url = anchor.href;
-  else
-    var url = document.location.href;
-
-  db(getTitle(url), thumbnailUrl);
-
-  console.log(s);
-  var bgImg = videoItem.querySelector(".thumb .bg-image");
-
-  if( !thumbnailUrl )
-    thumbnailUrl = "http://static.cdn.animeultima.tv/images/blank.gif";
-
-  var newBgImg = document.createElement("span");
-  newBgImg.setAttribute("style", "position: absolute; top: 1px; left: 1px; width: 150px; height: 100px; background-size: 150px 100px;");
-  newBgImg.style.backgroundImage = "url(" + thumbnailUrl + ")";
-  newBgImg.className = "newThumbnail";
-
-  bgImg.parentElement.appendChild(newBgImg);
-  bgImg.style.display = "none";
-
-  if( !s )
-    s = "Unknown";
-  var sourceTest = document.createElement("span");
-  sourceTest.className = "thumbnailSource";
-  sourceTest.innerText = s;
-  sourceTest.setAttribute("style", "background: black;font-size: 10px;color: #b1b1b1;position: absolute;bottom: 3px;left: 3px;z-index: 300;padding: 2px;")
-  bgImg.parentElement.appendChild(sourceTest);
-
-}
-
-
-function revert(){
+function revert() {
   
   var oldThumbnail = document.getElementsByClassName("bg-image");
   var newThumbnail = document.getElementsByClassName("newThumbnail");
@@ -151,27 +165,23 @@ function revert(){
   }
   for (var i = oldThumbnail.length - 1; i >= 0; i--) {
     oldThumbnail[i].removeAttribute("style");
-  }
-  for (var i = sources.length - 1; i >= 0; i--) {
     sources[i].parentElement.removeChild(sources[i]);
-  }
-  for (var i = newThumbnail.length - 1; i >= 0; i--) {
     newThumbnail[i].parentElement.removeChild(newThumbnail[i]);
   }
 }
 
 
-function db(title, imgUrl){
+function db(title, imgUrl) {
   var dbObj = aurdb.getDB("thumbnail-cache");
 
-  if( !dbObj )
+  if (!dbObj)
     dbObj = {};
 
-  if( dbObj[title] !== undefined && imgUrl === undefined ){
+  if (dbObj[title] !== undefined && imgUrl === undefined) {
     
     return dbObj[title];
   
-  }else if( imgUrl !== undefined ){
+  } else if (imgUrl !== undefined) {
     
     dbObj[title] = imgUrl;
     aurdb.setDB("thumbnail-cache", dbObj);
@@ -179,16 +189,16 @@ function db(title, imgUrl){
   }
 }
 
-function getTitle(url){
+function getTitle(url) {
   return url.replace(/^https?:\/\/(?:www\.)?animeultima\.io\/+([^]+-episode-[\d\.]+)(?:-english-[sd]ubbed(?:-video-mirror-\d+-[^]+)?)?(?:\/+)?(?:#[^]+)?$/, "$1");
 }
 
 
-function ini(){
-  if(page.isHome){
+function ini() {
+  if (page.isHome) {
     getThumbsForMainPage(document.getElementById("new-episodes"));
     getThumbsForMainPage(document.querySelector("#main-content-hp > div.section"));
-  }else if (page.isEpisode){
+  } else if (page.isEpisode) {
     getThumbsForMainPage(document.getElementById("related-videos"));  
   }
 }
